871479fe16224b99b658182539875c156802a4e5
Alex Stelea <alex.s@rdx.works>
2024-01-10 15:02:46 +0800

    build(core): add commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": () => [0, "always", 150],
+    "scope-enum": () => [
+      2,
+      "always",
+      ["web", "notification", "stream", "workers", "db", "core"],
+    ],
+    "scope-empty": [2, "never"],
+    "type-enum": () => [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
+};
