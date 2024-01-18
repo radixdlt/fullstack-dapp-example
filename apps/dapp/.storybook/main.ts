7403188a52e60bf4b9c610a2f9e8130b803d5055
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2024-01-18 10:00:35 +0000

    chore(dapp): add svelte csf

--- apps/dapp/.storybook/main.ts
+++ apps/dapp/.storybook/main.ts
@@ -1,6 +1,6 @@
 import type { StorybookConfig } from '@storybook/sveltekit';
-
 import { join, dirname } from 'path';
+import { mergeConfig, searchForWorkspaceRoot } from 'vite';
 
 /**
  * This function is used to resolve the absolute path of a package.
@@ -10,11 +10,12 @@ function getAbsolutePath(value: string): any {
 	return dirname(require.resolve(join(value, 'package.json')));
 }
 const config: StorybookConfig = {
-	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+	stories: ['../src/**/*.stories.svelte'],
 	addons: [
 		getAbsolutePath('@storybook/addon-links'),
 		getAbsolutePath('@storybook/addon-essentials'),
-		getAbsolutePath('@storybook/addon-interactions')
+		getAbsolutePath('@storybook/addon-interactions'),
+		'@storybook/addon-svelte-csf'
 	],
 	framework: {
 		name: getAbsolutePath('@storybook/sveltekit'),
