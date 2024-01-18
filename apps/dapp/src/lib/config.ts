b4313adcc6f8f612c83f7b32a3f685f9d3fb168d
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2024-01-18 05:12:49 +0000

    fix(dapp): development env

--- apps/dapp/src/lib/config.ts
+++ apps/dapp/src/lib/config.ts
@@ -1,23 +1,38 @@
+import {
+	EXPECTED_ORIGIN,
+	JWT_SECRET,
+	POSTGRES_DATABASE,
+	POSTGRES_HOST,
+	POSTGRES_PASSWORD,
+	POSTGRES_PORT,
+	POSTGRES_USER
+} from '$env/static/private';
+import {
+	PUBLIC_DAPP_DEFINITION_ADDRESS,
+	PUBLIC_LOG_LEVEL,
+	PUBLIC_NETWORK_ID
+} from '$env/static/public';
+
 export type Config = typeof config;
 
 export const config = {
 	jwt: {
-		secret: process.env.JWT_SECRET!,
+		secret: JWT_SECRET!,
 		refreshToken: { expiresIn: '30d', expiresInMs: 1000 * 60 * 60 * 24 * 30, key: 'jwt' },
 		authToken: { expiresIn: '10m' }
 	},
 	challenge: { expiresInMs: 600_000, byteLength: 32 },
 	postgres: {
-		database: process.env.POSTGRES_DATABASE!,
-		host: process.env.POSTGRES_HOST!,
-		user: process.env.POSTGRES_USER!,
-		password: process.env.POSTGRES_PASSWORD!,
-		port: parseInt(process.env.POSTGRES_PORT!, 10)
+		database: POSTGRES_DATABASE,
+		host: POSTGRES_HOST,
+		user: POSTGRES_USER,
+		password: POSTGRES_PASSWORD,
+		port: parseInt(POSTGRES_PORT, 10)
 	},
 	dapp: {
-		expectedOrigin: process.env.EXPECTED_ORIGIN!,
-		networkId: parseInt(process.env.PUBLIC_NETWORK_ID!, 10),
-		dAppDefinitionAddress: process.env.PUBLIC_DAPP_DEFINITION_ADDRESS!
+		expectedOrigin: EXPECTED_ORIGIN,
+		networkId: parseInt(PUBLIC_NETWORK_ID, 10),
+		dAppDefinitionAddress: PUBLIC_DAPP_DEFINITION_ADDRESS
 	},
-	logLevel: process.env.PUBLIC_LOG_LEVEL
+	logLevel: PUBLIC_LOG_LEVEL
 };
