29009af7a71400ebe177133881ffa3a0913debfb
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2024-01-18 10:01:58 +0000

    refactor(dapp): add storybook viewports

--- apps/dapp/.storybook/preview.ts
+++ apps/dapp/.storybook/preview.ts
@@ -9,6 +9,24 @@ const preview: Preview = {
 				color: /(background|color)$/i,
 				date: /Date$/i
 			}
+		},
+		viewport: {
+			viewports: {
+				'14" laptop': {
+					name: '14" laptop',
+					styles: {
+						width: '1600px',
+						height: '900px'
+					}
+				},
+				mobile: {
+					name: 'mobile',
+					styles: {
+						width: '393px',
+						height: '900px'
+					}
+				}
+			}
 		}
 	}
 };
