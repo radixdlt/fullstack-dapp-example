243dc2360733b1afadddc0ae8542503f0c9d4dff
Dawid Sowa <dawid.sowa@rdx.works>
2024-01-31 16:34:08 +0100

    feat(dapp): load glossary from md files

---
+++ apps/dapp/src/vite-plugins/helpers/tsPrinter.ts
@@ -0,0 +1,11 @@
+import ts from 'typescript'
+
+const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed })
+
+export const tsPrinter = (node: ts.Node) => {
+	return printer.printNode(
+		ts.EmitHint.Unspecified,
+		node,
+		ts.createSourceFile('', '', ts.ScriptTarget.Latest)
+	)
+}
--- apps/dapp/vite.config.ts
+++ apps/dapp/vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vitest/config'
+import { virtualGlossary } from './src/vite-plugins/virtualGlossary'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), virtualGlossary()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	}
