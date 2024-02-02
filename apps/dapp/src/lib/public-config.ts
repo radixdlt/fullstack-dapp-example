6752dee4b5d4fc03538b210f980cdd2acc3f8be5
Alex Stelea <alex.s@rdx.works>
2024-02-02 16:48:41 +0800

    build(dapp): use process env

--- apps/dapp/src/lib/public-config.ts
+++ apps/dapp/src/lib/public-config.ts
@@ -1,14 +1,8 @@
-import { env as publicEnv } from '$env/dynamic/public'
-
-const { PUBLIC_NETWORK_ID } = publicEnv
-
-const environment = {
-  ['2']: {
-    dAppDefinitionAddress: 'account_tdx_2_12x3rn7tqqqm3wguz6kmg5fy7saf8v92lt5xuwgn6kgh8zaejlf80ce'
-  }
-}[PUBLIC_NETWORK_ID]
+const { PUBLIC_NETWORK_ID } = process.env
 
 export const publicConfig = {
-  networkId: parseInt(PUBLIC_NETWORK_ID),
-  dAppDefinitionAddress: environment?.dAppDefinitionAddress || ''
-}
+  ['2']: {
+    dAppDefinitionAddress: 'account_tdx_2_12x3rn7tqqqm3wguz6kmg5fy7saf8v92lt5xuwgn6kgh8zaejlf80ce',
+    networkId: parseInt(PUBLIC_NETWORK_ID!)
+  }
+}[PUBLIC_NETWORK_ID || '2']!
