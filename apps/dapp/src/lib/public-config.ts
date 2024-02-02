49e9c85f910df712a9e9184fc772eacfb557f153
Alex Stelea <alex.s@rdx.works>
2024-02-02 17:03:51 +0800

    build(dapp): static/public env

--- apps/dapp/src/lib/public-config.ts
+++ apps/dapp/src/lib/public-config.ts
@@ -1,8 +1,8 @@
-const { PUBLIC_NETWORK_ID } = process.env
+import { PUBLIC_NETWORK_ID } from '$env/static/public'
 
 export const publicConfig = {
   ['2']: {
     dAppDefinitionAddress: 'account_tdx_2_12x3rn7tqqqm3wguz6kmg5fy7saf8v92lt5xuwgn6kgh8zaejlf80ce',
     networkId: parseInt(PUBLIC_NETWORK_ID!)
   }
-}[PUBLIC_NETWORK_ID || '2']!
+}[PUBLIC_NETWORK_ID]!
