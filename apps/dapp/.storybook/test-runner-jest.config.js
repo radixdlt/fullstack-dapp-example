f4a2fc6e9e406d58f393fe377feb3250d5b2ec59
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2024-02-13 10:23:48 +0000

    chore(dapp): format

--- apps/dapp/.storybook/test-runner-jest.config.js
+++ apps/dapp/.storybook/test-runner-jest.config.js
@@ -1,20 +1,20 @@
-const { getJestConfig } = require('@storybook/test-runner');
+const { getJestConfig } = require('@storybook/test-runner')
 
-const testRunnerConfig = getJestConfig();
+const testRunnerConfig = getJestConfig()
 
 /**
  * @type {import('@jest/types').Config.InitialOptions}
  */
 module.exports = {
-    // The default Jest configuration comes from @storybook/test-runner
-    ...testRunnerConfig,
-    /** Add your own overrides below
-     * @see https://jestjs.io/docs/configuration
-     */
-    testEnvironmentOptions: {
-        'jest-playwright': {
-            devices: ['iPhone 11', 'Desktop Chrome'],
-        }
-    },
-    testTimeout: 20000, // default timeout is 15s
-};
+  // The default Jest configuration comes from @storybook/test-runner
+  ...testRunnerConfig,
+  /** Add your own overrides below
+   * @see https://jestjs.io/docs/configuration
+   */
+  testEnvironmentOptions: {
+    'jest-playwright': {
+      devices: ['iPhone 11', 'Desktop Chrome']
+    }
+  },
+  testTimeout: 20000 // default timeout is 15s
+}
--- apps/dapp/src/markdown/quests/en/RadQuest.md
+++ apps/dapp/src/markdown/quests/en/RadQuest.md
@@ -8,6 +8,6 @@ Bridge
 Staking
 Multi-factor security setup
 
-Any time you want to move web3 assets around on the Radar Network, it has to submit a transaction to the network. 
+Any time you want to move web3 assets around on the Radar Network, it has to submit a transaction to the network.
 
 You will see the transaction in your Radar wallet so you can read it, understand what it will do, and choose if you want to securely "sign" it.
