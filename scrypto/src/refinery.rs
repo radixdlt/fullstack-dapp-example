eed330df6f09e04dcf1a5fd3ccd7477a6920afcb
Bryan <motosharpley@gmail.com>
2024-02-26 08:10:27 -0800

    feat(scrypto): add method auth, user_id proofs & event fields, remove radgem creation, set owner role

--- scrypto/src/refinery.rs
+++ scrypto/src/refinery.rs
@@ -1,31 +1,31 @@
 use scrypto::prelude::*;
 
-#[derive(ScryptoSbor, NonFungibleData)]
-struct RadGem {
-    name: String,
-    color: String,
-    material: String,
-    rarity: String,
-}
-
-#[derive(ScryptoSbor, ScryptoEvent)]
-struct RegisteredEvent {
-    event: String,
-}
+#[derive(ScryptoSbor, PartialEq, Eq, PartialOrd, Ord, Hash, Debug, Clone)]
+#[sbor(transparent)]
+pub struct UserId(String);
 
 #[blueprint]
 #[events(
-    ElelmentsCombineDepositedEvent,
-    ElelmentsCombineProccessedEvent,
-    ElelmentsCombineClaimedEvent,
+    ElementsCombineDepositedEvent,
+    ElementsCombineProccessedEvent,
+    ElementsCombineClaimedEvent,
     RadgemsTransformedEvent
 )]
 mod refinery {
+    enable_method_auth! {
+        roles {
+            admin => updatable_by: [OWNER];
+        },
+        methods {
+            combine_elements_deposit => PUBLIC;
+            combine_elements_process => restrict_to: [admin];
+            combine_elements_claim => PUBLIC;
+            transform_radgems => PUBLIC;
+        }
+    }
     struct Refinery {
-        radgem_resource_manager: ResourceManager,
-        // radgem_deposit: KeyValueStore<ClaimTicketNftId, Vault>,
-        radgem_vault: Vault,
-        admin_badge: ResourceAddress,
+        admin_badge_address: ResourceAddress,
+        user_badge_address: ResourceAddress,
     }
 
     impl Refinery {
@@ -35,70 +35,79 @@ mod refinery {
             admin_badge_address: ResourceAddress,
             user_badge_address: ResourceAddress,
         ) -> Global<Refinery> {
-            // Create RadGem NFT
-            let radgem_resource_manager =
-                ResourceBuilder::new_ruid_non_fungible::<RadGem>(OwnerRole::None)
-                    .mint_roles(mint_roles!(
-                        minter => rule!(require(admin_badge_address));
-                        minter_updater => rule!(deny_all);
-                    ))
-                    .create_with_no_initial_supply();
-
             Self {
-                radgem_resource_manager: radgem_resource_manager,
-                radgem_vault: Vault::new(radgem_resource_manager.address()),
-                // radgem_deposit: KeyValueStore::new(),
-                admin_badge: admin_badge_address,
+                admin_badge_address,
+                user_badge_address,
             }
             .instantiate()
-            .prepare_to_globalize(OwnerRole::None)
+            .prepare_to_globalize(OwnerRole::Fixed(rule!(require(owner_badge_address))))
             .globalize()
         }
 
         // combine_elements_deposit create a function that takes a Bucket of elemtents and burns them
         // emit elements_combine_deposited event
-        fn combine_elements_deposit(&self, elements: Bucket) -> () {
+        pub fn combine_elements_deposit(&self, elements: Bucket, user_badge: Proof) -> () {
+            let user_badge = user_badge.check(self.user_badge_address);
+            let user_id = UserId(
+                user_badge
+                    .as_non_fungible()
+                    .non_fungible_local_id()
+                    .to_string(),
+            );
             // burn the elements
             elements.burn();
-            Runtime::emit_event(ElelmentsCombineDepositedEvent {
+            Runtime::emit_event(ElementsCombineDepositedEvent {
                 event: "elements_combine_deposited".to_string(),
+                user_id,
             });
         }
 
         // combine_elements_process Mint a Random RadGem NFT
         // emit elements_combine_proccessed event
-        pub fn combine_elements_process(&mut self, elements: Bucket) -> () {
-            // Burn the elements
-            self.combine_elements_deposit(elements);
-            // Mint a RadGem
-            let radgem_bucket = self.radgem_resource_manager.mint_ruid_non_fungible(RadGem {
-                name: "RadGem".to_string(),
-                color: "Red".to_string(),
-                material: "radiant".to_string(),
-                rarity: "common".to_string(),
-            });
-            self.radgem_vault.put(radgem_bucket);
+        pub fn combine_elements_process(&mut self, user_id: UserId) -> () {
             // TODO Store the User ID and the RadGem ID in a KeyValueStore
+            // TODO Mint a RadGem
 
             // Emit the event
-            Runtime::emit_event(ElelmentsCombineProccessedEvent {
+            Runtime::emit_event(ElementsCombineProccessedEvent {
                 event: "elements_combine_proccessed".to_string(),
+                user_id,
             });
         }
         // combine_elements_claim create a function to claim the RadGem
         // emit elements_combine_claimed event
-        pub fn combine_elements_claim(&mut self, user_id: String, radgem_id: String) -> () {
+        pub fn combine_elements_claim(&mut self, user_badge: Proof) -> () {
             // TODO Get the RadGem from the Vault
             // TODO Transfer the RadGem to the User
             // TODO Remove the User ID and the RadGem ID from the KeyValueStore or mark it as claimed
-            Runtime::emit_event(ElelmentsCombineClaimedEvent {
+            let user_badge = user_badge.check(self.user_badge_address);
+            let user_id = UserId(
+                user_badge
+                    .as_non_fungible()
+                    .non_fungible_local_id()
+                    .to_string(),
+            );
+            Runtime::emit_event(ElementsCombineClaimedEvent {
                 event: "elements_combine_claimed".to_string(),
+                user_id,
             });
         }
 
         // transform_radgems create a function to transform RadGems & Morph Card into a new RadMorph NFT
         // emit radgems_transformed event
-        pub fn transform_radgems(&mut self, radgems: Bucket, morph_card: Bucket) -> () {
+        pub fn transform_radgems(
+            &mut self,
+            radgems: Bucket,
+            morph_card: Bucket,
+            user_badge: Proof,
+        ) -> () {
+            let user_badge = user_badge.check(self.user_badge_address);
+            let user_id = UserId(
+                user_badge
+                    .as_non_fungible()
+                    .non_fungible_local_id()
+                    .to_string(),
+            );
             // Burn the RadGems
             radgems.burn();
             // Burn the Morph Card
@@ -108,27 +117,32 @@ mod refinery {
             // Emit the event
             Runtime::emit_event(RadgemsTransformedEvent {
                 event: "radgems_transformed".to_string(),
+                user_id,
             });
         }
     }
 }
 
 #[derive(ScryptoSbor, ScryptoEvent)]
-pub struct ElelmentsCombineDepositedEvent {
+pub struct ElementsCombineDepositedEvent {
     event: String,
+    user_id: UserId,
 }
 
 #[derive(ScryptoSbor, ScryptoEvent)]
-pub struct ElelmentsCombineProccessedEvent {
+pub struct ElementsCombineProccessedEvent {
     event: String,
+    user_id: UserId,
 }
 
 #[derive(ScryptoSbor, ScryptoEvent)]
-pub struct ElelmentsCombineClaimedEvent {
+pub struct ElementsCombineClaimedEvent {
     event: String,
+    user_id: UserId,
 }
 
 #[derive(ScryptoSbor, ScryptoEvent)]
 pub struct RadgemsTransformedEvent {
     event: String,
+    user_id: UserId,
 }
