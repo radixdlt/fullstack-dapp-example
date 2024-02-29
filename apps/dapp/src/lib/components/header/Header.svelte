b332754ca81771bd3f70a02bf60de17e1bb3b423
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2024-02-29 19:19:24 +0000

    fix(dapp): header icon

--- apps/dapp/src/lib/components/header/Header.svelte
+++ apps/dapp/src/lib/components/header/Header.svelte
@@ -1,26 +1,33 @@
 <script lang="ts">
+  import { i18n } from '$lib/i18n'
   import RadquestLogoBig from '@images/radquest-logo-big.svg'
   import RadquestLogoSmall from '@images/radquest-logo-small.svg'
-
-  let width: number
-
-  $: mobile = width < 768
 </script>
 
-<svelte:window bind:innerWidth={width} />
-
 <div class="header">
-  <img src={mobile ? RadquestLogoSmall : RadquestLogoBig} alt="Radquest Logo" />
+  <img
+    style:--big-logo={`url(${RadquestLogoBig})`}
+    style:--small-logo={`url(${RadquestLogoSmall})`}
+    alt={$i18n.t('radquest_logo_alt')}
+  />
   <radix-connect-button class="radix-btn" />
 </div>
 
-<style>
+<style lang="scss">
   .header {
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: var(--spacing-lg);
     height: 4rem;
+  }
+
+  img {
+    content: var(--big-logo);
+
+    @include mobile {
+      content: var(--small-logo);
+    }
   }
 
   .radix-btn {
