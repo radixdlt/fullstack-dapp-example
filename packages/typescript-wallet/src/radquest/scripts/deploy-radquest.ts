a6de418a9a09c20abd0ec116c364d8d6f8ba84d9
Dawid Sowa <dawid.sowa@rdx.works>
2024-03-07 13:25:01 +0100

    ci(core): add lint dependency

--- packages/typescript-wallet/src/radquest/scripts/deploy-radquest.ts
+++ packages/typescript-wallet/src/radquest/scripts/deploy-radquest.ts
@@ -3,8 +3,8 @@ import { existsSync, readFileSync } from 'fs'
 import { publishPackageAdvanced } from '../../helpers/publish-package-advanced'
 import { config } from '../../config'
 
-const rpdPath = '../../scrypto/target/wasm32-unknown-unknown/release/radquest.rpd'
-const wasmPath = '../../scrypto/target/wasm32-unknown-unknown/release/radquest.wasm'
+const rpdPath = '../../scrypto-package/target/wasm32-unknown-unknown/release/radquest.rpd'
+const wasmPath = '../../scrypto-package/target/wasm32-unknown-unknown/release/radquest.wasm'
 
 if (!existsSync(resolve(rpdPath)) || !existsSync(resolve(wasmPath))) {
   throw new Error('rpd and wasm files not found')
