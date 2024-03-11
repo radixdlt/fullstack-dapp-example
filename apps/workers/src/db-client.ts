7cd2b1251700b3c8b48f81950f06bde305493519
Alex Stelea <alex.s@rdx.works>
2024-03-11 19:05:24 +0000

    refactor(workers): instantiate db client

--- apps/workers/src/db-client.ts
+++ apps/workers/src/db-client.ts
@@ -1,8 +1,16 @@
+import { appLogger } from 'common'
 import { config } from './config'
 import { PrismaClient } from 'database'
 
 const { user, password, host, port, database } = config.postgres
 
-export const dbClient = new PrismaClient({
-  datasourceUrl: `postgresql://${user}:${password}@${host}:${port}/${database}?schema=public`
-})
+export const DbClient = async () => {
+  const db = new PrismaClient({
+    datasourceUrl: `postgresql://${user}:${password}@${host}:${port}/${database}?schema=public`
+  })
+
+  await db.event.findFirst()
+  appLogger.debug({ method: 'DbClient', event: 'ConnectionSuccess' })
+
+  return db
+}
