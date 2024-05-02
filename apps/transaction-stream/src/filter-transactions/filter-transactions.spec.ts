af8fdf0b54fa7631757e5b831175ae9342836468
Marcin Moldrzyk <nostromo@marcins-mbp.lan>
2024-05-02 19:13:27 +0100

    feat(dapp): add tests for filtering transactions and add unsupported tx to them

--- apps/transaction-stream/src/filter-transactions/filter-transactions.spec.ts
+++ apps/transaction-stream/src/filter-transactions/filter-transactions.spec.ts
@@ -2,8 +2,13 @@ import { describe, it, expect } from 'vitest'
 import { FilterTransactions } from './filter-transactions'
 import DepositUserBadge from '../fixtures/transactions/deposit-user-badge'
 import QuestRewardsEvents from '../fixtures/transactions/quest-rewards-events'
-// import StakedXrdTx from '../fixtures/transactions/staked-xrd'
-import { getTrackedTransactionTypes } from './tracked-transaction-types'
+import NotSupportedTx from '../fixtures/transactions/not-supported-tx'
+import StakedXrdTx from '../fixtures/transactions/staked-xrd'
+import {
+  getTrackedTransactionTypes,
+  resourceStaked,
+  resourceWithdrawn
+} from './tracked-transaction-types'
 import { RedisConnection } from 'queues'
 import { config } from '../config'
 import { ActiveQuestsModel } from 'common'
@@ -15,7 +20,7 @@ const filterTransactions = FilterTransactions(trackedTransactionTypes, activeQue
 
 describe('filter transactions', () => {
   it('should find DepositUserBadge transaction', async () => {
-    const result = await filterTransactions(DepositUserBadge)
+    const result = await filterTransactions([...DepositUserBadge, ...NotSupportedTx])
 
     if (result.isErr()) throw result.error
 
@@ -32,7 +37,7 @@ describe('filter transactions', () => {
   })
 
   it('should find QuestRewardClaimed & QuestRewardDeposited transaction', async () => {
-    const result = await filterTransactions(QuestRewardsEvents)
+    const result = await filterTransactions([...QuestRewardsEvents, ...NotSupportedTx])
 
     if (result.isErr()) throw result.error
 
@@ -52,20 +57,27 @@ describe('filter transactions', () => {
   })
 
   it('should find XrdStaked transaction', async () => {
-    // const addActiveQuestResult = await activeQuestsModel
-    //   .addActiveQuestAccount(
-    //     'account_tdx_2_12ys6rt7m4zsut5fpm77melt0wl3kj659vv59xzm4dduqtqse4fv7wa',
-    //     'StakingQuest'
-    //   )
-    //   .map(() => console.log('OK'))
-    //   .mapErr(() => console.error('SDSDDSDDS'))
-    // console.log(addActiveQuestResult)
-    // expect(addActiveQuestResult.isOk()).toBe(true)
-    // const result = filterTransactions(StakedXrdTx)
-    // if (result.isErr()) throw result.error
-    // const filteredTransactions = result.value
-    // expect(filteredTransactions.length).toEqual(2)
-    // const [xrdStaked, withdraw] = filteredTransactions
-    // console.log(xrdStaked, withdraw)
-  }, 15000)
+    const stakingAccount = 'account_tdx_2_12ys6rt7m4zsut5fpm77melt0wl3kj659vv59xzm4dduqtqse4fv7wa'
+
+    const addActiveQuestResult = await activeQuestsModel.addActiveQuestAccount(
+      stakingAccount,
+      'StakingQuest'
+    )
+
+    if (addActiveQuestResult.isErr()) throw addActiveQuestResult.error
+
+    const filterResult = await filterTransactions([...StakedXrdTx, ...NotSupportedTx])
+
+    if (filterResult.isErr()) throw filterResult.error
+
+    const filteredTransactions = filterResult.value
+    expect(filteredTransactions).lengthOf(1)
+
+    const relevantEvents = Object.values(filteredTransactions[0].relevantEvents)
+    expect(relevantEvents).lengthOf(2)
+
+    const [withdraw, xrdStaked] = relevantEvents
+    expect(withdraw.name).toBe('WithdrawEvent')
+    expect(xrdStaked.name).toBe('StakeEvent')
+  })
 })
