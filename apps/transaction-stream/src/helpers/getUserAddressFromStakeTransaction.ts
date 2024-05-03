3cd60aceb04a49d1fe5014e5d445da12e76c3253
Marcin Moldrzyk <nostromo@marcins-mbp.lan>
2024-05-03 16:54:22 +0100

    fix(dapp): remove unused transaction stream helper

--- apps/transaction-stream/src/helpers/getUserAddressFromStakeTransaction.ts
+++
@@ -1,9 +0,0 @@
-import { EventsItem } from '@radixdlt/babylon-gateway-api-sdk'
-import { resourceWithdrawn } from '../filter-transactions/tracked-transaction-types'
-
-export const getUserAddressFromStakingTransaction =
-  (resource: string) =>
-  (events: EventsItem[]): string | undefined => {
-    const withdrawEvent = events.find(resourceWithdrawn(resource))
-    return (withdrawEvent?.emitter as any).entity.entity_address
-  }
