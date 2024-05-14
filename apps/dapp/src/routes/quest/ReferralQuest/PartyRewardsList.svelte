853b1420c2c3695a99d72eab76c366db73984a98
Marcin Moldrzyk <nostromo@Marcins-MacBook-Pro.local>
2024-05-14 14:52:25 +0100

    code(dapp): add party rewards list to referral quest ui

---
+++ apps/dapp/src/routes/quest/ReferralQuest/PartyRewardsList.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { Quests } from 'content'
+
+  export let questId: keyof Quests
+
+  let tab: 'reward_tiers' | 'party_participants'
+</script>
+
+<div class="text-content">
+  <h3 class="title">Party rewards Status</h3>
+</div>
+
+<style lang="scss">
+  h3,
+  p {
+    margin: 0;
+  }
+  .text-content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: var(--spacing-lg);
+    text-align: center;
+    overflow: hidden;
+  }
+</style>
