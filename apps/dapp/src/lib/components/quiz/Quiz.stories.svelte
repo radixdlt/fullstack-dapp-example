6bab365c91982fe19dcdab0890f6a3f11f83ad9e
Dawid Sowa <dawid.sowa@rdx.works>
2024-05-16 12:44:18 +0200

    code(dapp): add quiz component

---
+++ apps/dapp/src/lib/components/quiz/Quiz.stories.svelte
@@ -0,0 +1,31 @@
+<script lang="ts" context="module">
+  import { Story, Template } from '@storybook/addon-svelte-csf'
+  import Quiz from './Quiz.svelte'
+  export const meta = {
+    tags: ['autodocs']
+  }
+</script>
+
+<Template>
+  <div style="padding: 1rem">
+    <Quiz
+      answers={[
+        {
+          text: 'the collective noun for 2000-tentacled giant squids',
+          correct: false
+        },
+        {
+          text: 'the scientific name for the hive of the mythical Radon Bumblebee.',
+          correct: false
+        },
+        {
+          text: 'a decentralized ecosystem of networked computer nodes designed to make accessing Web3 easier for developers and users.',
+          correct: true
+        }
+      ]}
+      on:correctAnswer={() => console.log('correct answer selected')}
+    />
+  </div>
+</Template>
+
+<Story name="Primary" />
