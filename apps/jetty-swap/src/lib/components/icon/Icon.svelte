900ec60460dcfe82ebd08e4670e541ac1d5eed27
Marcin Moldrzyk <nostromo@marcins-mbp.lan>
2024-05-27 11:32:47 +0100

    fix(jetty-swap): render placeholder if url in icon is not provided

--- apps/jetty-swap/src/lib/components/icon/Icon.svelte
+++ apps/jetty-swap/src/lib/components/icon/Icon.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
-  export let url: string
+  export let url: string | undefined
   export let size: 'small' | 'medium' | 'large' = 'small'
   export let clickable = false
 </script>
 
 <div class="icon" role="button" tabindex="0" on:keydown class:clickable on:click>
-  <img class={size} src={url} alt="icon" />
+  {#if url}
+    <img class={size} src={url} alt="icon" />
+  {:else}
+    <div class={size} />
+  {/if}
   {#if $$slots['default']}
     <div class="text">
       <slot />
