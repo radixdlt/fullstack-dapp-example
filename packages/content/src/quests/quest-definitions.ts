828849aafcaeac76c3e66f335f597f68c2ce8a34
Marcin <marcin.moldrzyk@rdx.works>
2024-06-06 12:30:21 +0200

    code(content): remove letty swap requirement from quest-definitions

--- packages/content/src/quests/quest-definitions.ts
+++ packages/content/src/quests/quest-definitions.ts
@@ -240,10 +240,6 @@ export const QuestDefinitions = () => {
         [EventId.JettySwap]: {
           eventName: 'JettySwapEvent',
           type: 'event'
-        },
-        LettySwap: {
-          eventName: 'LettySwapEvent',
-          type: 'event'
         }
       }
     },
