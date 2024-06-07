44cb45ef3fdc1a5205a242f1ef117dd849c3e46a
Dawid Sowa <dawid.sowa@rdx.works>
2024-06-07 17:05:17 +0200

    fix(dapp): bring back /connect route

---
+++ apps/dapp/src/routes/connect/+layout.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import '../../global.scss'
+  import { onMount } from 'svelte'
+  import { RadixDappToolkit, Logger } from '@radixdlt/radix-dapp-toolkit'
+  import { publicConfig } from '$lib/public-config'
+
+  let radixDappToolkit: RadixDappToolkit
+
+  const { dAppDefinitionAddress, networkId } = publicConfig
+
+  onMount(() => {
+    const logger = Logger(1)
+
+    radixDappToolkit = RadixDappToolkit({
+      networkId,
+      dAppDefinitionAddress: dAppDefinitionAddress ?? '',
+      logger,
+      featureFlags: ['ExperimentalMobileSupport']
+    })
+  })
+</script>
