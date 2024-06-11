b377514eaea747669e3b5712bac3eb27d9b98a04
Marcin <marcin.moldrzyk@rdx.works>
2024-06-11 15:26:10 +0200

    code(jetty-swap): load themes without on mount

--- apps/jetty-swap/src/routes/+layout.svelte
+++ apps/jetty-swap/src/routes/+layout.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
   import { PUBLIC_SWAP_VARIATION } from '$env/static/public'
-  import { onMount } from 'svelte'
   import '../global.scss'
   import Nav from './Nav.svelte'
 
-  onMount(async () => {
+  const loadTheme = async () => {
     if (PUBLIC_SWAP_VARIATION === 'JETTY') {
       await import('../jetty.scss')
     } else {
       await import('../letty.scss')
     }
-  })
+  }
+
+  loadTheme()
 </script>
 
 <svelte:head>
