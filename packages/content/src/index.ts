3cfd5232f9b7fcdc725831aff86a3d10504890f2
Marcin <marcin.moldrzyk@rdx.works>
2024-06-18 17:33:46 +0200

    code(content): add landing popup markdown

--- packages/content/src/index.ts
+++ packages/content/src/index.ts
@@ -3,3 +3,6 @@ export * from './quests/quest-definitions'
 
 export * from './glossary/load-glossary'
 export * from './glossary/glossary-definitions'
+
+export * from './landingPopup/load-landing-popup'
+export * from './landingPopup/landing-popup-definitions'
---
+++ packages/content/src/landingPopup/load-landing-popup.ts
@@ -0,0 +1,14 @@
+import { Language } from '..'
+import { LandingPopupDefinitions, landingPopupIds } from './landing-popup-definitions'
+import LandingPopupIndex from './landing-popup-index.json'
+
+export const loadLandingPopup = (language: Language) =>
+  landingPopupIds.map((id) => {
+    const landingPopupDefinition = LandingPopupDefinitions[id][language]
+    const html = Object.values(LandingPopupIndex[id][language])[0]
+
+    if (!landingPopupDefinition)
+      throw new Error(`Language '${language}' is not supported for landing page`)
+
+    return { id, ...landingPopupDefinition, html }
+  })
