573c4d2474616371311ff6814413d5421066df1e
Alex Stelea <alex.s@rdx.works>
2024-06-24 17:11:12 +0100

    code: ignore global crypto

--- packages/common/src/user/get-random-referral-code.ts
+++ packages/common/src/user/get-random-referral-code.ts
@@ -1,10 +1,9 @@
-import { getRandomValues } from 'node:crypto'
-
 export const getRandomReferralCode = (length: number = 6) => {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
   let result = ''
   const randomArray = new Uint8Array(length)
-  getRandomValues(randomArray)
+  // @ts-ignore
+  crypto.getRandomValues(randomArray)
   randomArray.forEach((number) => {
     result += chars[number % chars.length]
   })
