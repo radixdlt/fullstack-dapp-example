490792db7de88c4c4dc1dc9027b6b25b28bbeca8
Alex Stelea <alex.s@rdx.works>
2024-06-25 15:01:54 +0100

    code(dapp): set email page

--- apps/dapp/src/routes/home/[category=quest_category]/quest/GetRadixWallet/SetEmailPage.svelte
+++ apps/dapp/src/routes/home/[category=quest_category]/quest/GetRadixWallet/SetEmailPage.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-  import JettyActionButtons from '$lib/components/quest/JettyActionButtons.svelte'
   import EnterEmail from './EnterEmail.svelte'
 
   export let text: string
-  export let onBack: () => void
-  export let onNext: () => void
 
   let email: string
   let sendNewsletter = false
@@ -13,5 +10,3 @@
 {@html text}
 
 <EnterEmail bind:email bind:checked={sendNewsletter} />
-
-<JettyActionButtons on:back={onBack} on:next={onNext} />
