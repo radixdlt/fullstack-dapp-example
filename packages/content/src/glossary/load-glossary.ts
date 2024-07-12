c4f5e26920a1997fe09b09cc2b44e41c5dc44b05
Dawid Sowa <dawid.sowa@rdx.works>
2024-07-12 14:09:21 +0200

    fix(content): add glossary alphabetical sorting order

--- packages/content/src/glossary/load-glossary.ts
+++ packages/content/src/glossary/load-glossary.ts
@@ -3,12 +3,14 @@ import { GlossaryDefinitions, glossaryKeys } from './glossary-definitions'
 import GlossaryIndex from './glossary-index.json'
 
 export const loadGlossary = (language: Language) =>
-  glossaryKeys.map((glossaryId) => {
-    const glossaryDefinition = GlossaryDefinitions[glossaryId][language]
-    const html = Object.values(GlossaryIndex[glossaryId][language])[0]
+  glossaryKeys
+    .map((glossaryId) => {
+      const glossaryDefinition = GlossaryDefinitions[glossaryId][language]
+      const html = Object.values(GlossaryIndex[glossaryId][language])[0]
 
-    if (!glossaryDefinition)
-      throw new Error(`Language '${language}' is not supported for glossary: '${glossaryId}'`)
+      if (!glossaryDefinition)
+        throw new Error(`Language '${language}' is not supported for glossary: '${glossaryId}'`)
 
-    return { id: glossaryId, ...glossaryDefinition, html }
-  })
+      return { id: glossaryId, ...glossaryDefinition, html }
+    })
+    .sort((a, b) => a.title.localeCompare(b.title))
