06b3d1fcee953315a8a69fedfe9c22e7917ccf57
AbstractFruitFactory <abstractfruitfactory@gmail.com>
2024-07-17 12:35:09 +0100

    fix(dapp): fix copy

--- apps/dapp/src/routes/home/OpenGiftBox.svelte
+++ apps/dapp/src/routes/home/OpenGiftBox.svelte
@@ -108,7 +108,7 @@
       imageUrl: string
     }
   }
-  let giftBoxName: string
+
   let rewards: {
     amountOfElements: number
     cardData: {
@@ -216,6 +216,7 @@
 
   const openGiftBox = (giftBoxAddress: string) => {
     waitingForOpenTransaction = true
+
     sendTransaction({
       transactionManifest: openGiftBoxManifest(giftBoxAddress)
     })
@@ -323,9 +324,9 @@
     </div>
   {:else if waitingForDepositedRewards}
     <JettyMenuItemPage>
-    <div class="title">
-      {$i18n.t('jetty:open-gift-box.opening-gift-box')}...
-    </div>
+      <div class="title">
+        {$i18n.t('jetty:open-gift-box.opening-gift-box')}...
+      </div>
 
       <lottie-player
         autoplay
@@ -347,7 +348,7 @@
     >
       <div class="header-text" slot="header">
         <div class="title">
-          {$i18n.t('jetty:open-gift-box.gift-box-opened-title', { name: giftBoxName })}
+          {$i18n.t('jetty:open-gift-box.gift-box-opened-title')}
         </div>
         <div class="subtitle">
           {$i18n.t('jetty:open-gift-box.gift-box-opened-subtitle')}...
@@ -428,7 +429,7 @@
                   }}
                 >
                   <div class="gift-box" style:--image={`url(${image})`} />
-                  <div slot="text">{giftBoxName || name}</div>
+                  <div slot="text">{name}</div>
                 </ResourceCard>
               </Item>
             {/if}
