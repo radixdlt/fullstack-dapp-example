b891bb8dd24a94122dbca615003ed705085dd1a8
Azizi <64193693+azizi-a@users.noreply.github.com>
2024-07-19 18:04:49 +0100

    code(jetty-swap): get clam price before wallet connect

--- apps/jetty-swap/src/lib/utils/previewTranasction.ts
+++ apps/jetty-swap/src/lib/utils/previewTranasction.ts
@@ -3,13 +3,12 @@ import { get } from 'svelte/store'
 import { createSwapManifest, type CreateSwapManifestProps } from './createSwapManifest'
 import type { GatewayApi } from 'common'
 
-export const previewTransaction = async (props: CreateSwapManifestProps) => {
+export const previewTransaction = async (manifest: string) => {
   const gatewayApi = get(GatewayAPI) as GatewayApi
-  const manifest = createSwapManifest(props)
   const status = await gatewayApi?.gatewayApiClient.status.getCurrent()
 
-  const currentEpoch = status.ledger_state.epoch
-  return gatewayApi.gatewayApiClient.transaction.innerClient.transactionPreview({
+  const currentEpoch = status?.ledger_state.epoch
+  return gatewayApi?.gatewayApiClient.transaction.innerClient.transactionPreview({
     transactionPreviewRequest: {
       manifest,
       start_epoch_inclusive: currentEpoch,
@@ -34,7 +33,7 @@ export const getTransactionResult = (
   const gatewayApi = get(GatewayAPI) as GatewayApi
   return gatewayApi.callApi('getCommittedDetails', transactionId).map((response) => {
     if (response?.transaction.balance_changes) {
-      const balanceChange = response?.transaction?.balance_changes.fungible_balance_changes.find(
+      const balanceChange = response?.transaction.balance_changes.fungible_balance_changes.find(
         (change) =>
           change.resource_address === toTokenAddress && change.entity_address === toAddress
       )
@@ -44,9 +43,23 @@ export const getTransactionResult = (
 }
 
 export const getBalanceChange = async (props: CreateSwapManifestProps, toTokenAddress: string) => {
-  const tx = await previewTransaction(props)
+  const manifest = createSwapManifest(props)
+  const tx = await previewTransaction(manifest)
   const balanceChange: any = tx.resource_changes.find(
     (change: any) => change.resource_changes[0]?.resource_address === toTokenAddress
   )
   return balanceChange?.resource_changes[0].amount as string
+}
+
+export const getPrice = async (dexComponent: string) => {
+  const manifest = `
+  CALL_METHOD
+    Address("${dexComponent}")
+    "get_price"
+  ;
+`
+  const tx = await previewTransaction(manifest)
+
+  const price: string | undefined = (tx as any)?.receipt?.output[0].programmatic_json.value
+  return price
 }
