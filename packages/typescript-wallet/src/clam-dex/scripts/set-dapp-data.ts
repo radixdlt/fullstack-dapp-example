789992832db17650ce88f847e5a9253ff8779cea
Azizi <64193693+azizi-a@users.noreply.github.com>
2024-07-22 19:53:26 +0100

    code(wallet): change some log levels to debug

--- packages/typescript-wallet/src/clam-dex/scripts/set-dapp-data.ts
+++ packages/typescript-wallet/src/clam-dex/scripts/set-dapp-data.ts
@@ -5,10 +5,10 @@ import { setLettyswapMetadata } from '../helpers/setLettyswapMetadata'
 const exec = async () => {
   const result1 = await setLettyswapMetadata()
   if (result1.isErr()) logger.error(result1.error)
-  else logger.info(result1.value)
+  else logger.debug(result1.value)
   const result2 = await setJettyswapMetadata()
   if (result2.isErr()) logger.error(result2.error)
-  else logger.info(result2.value)
+  else logger.debug(result2.value)
 }
 
 exec()
