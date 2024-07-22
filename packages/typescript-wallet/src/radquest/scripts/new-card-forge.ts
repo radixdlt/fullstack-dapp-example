93d48b8b747a269feb7e78511390bb92fbfac76d
Azizi <64193693+azizi-a@users.noreply.github.com>
2024-07-22 17:48:24 +0100

    code(wallet): remove stokenet addresses hardcoding

--- packages/typescript-wallet/src/radquest/scripts/new-card-forge.ts
+++ packages/typescript-wallet/src/radquest/scripts/new-card-forge.ts
@@ -1,14 +1,11 @@
-import { Addresses } from 'common'
 import { newCardForge } from '../helpers/newCardForge'
 import { mintAdminBadge } from '../helpers/mintAdminBadge'
 import { logger } from '../../helpers'
-import { addCardForgeCards } from '../helpers/addCardForeCards'
+import { config } from '../../config'
 
 mintAdminBadge({
-  adminBadgeAddress: Addresses(2).badges.adminBadgeAddress,
-  superAdminBadgeAddress: Addresses(2).badges.superAdminBadgeAddress
+  adminBadgeAddress: config.radQuest.badges.adminBadgeAddress,
+  superAdminBadgeAddress: config.radQuest.badges.superAdminBadgeAddress
 })
   .andThen(() => newCardForge())
-  .map(({ cardForgeAddress }) => {
-    addCardForgeCards(cardForgeAddress).map(() => logger.debug({ cardForgeAddress }))
-  })
+  .map((cardForgeAddress) => logger.debug(cardForgeAddress))
--- packages/typescript-wallet/src/radquest/scripts/new-hero-badge-forge.ts
+++ packages/typescript-wallet/src/radquest/scripts/new-hero-badge-forge.ts
@@ -1,11 +1,11 @@
 import { newHeroBadgeForge } from '../helpers/newHeroBadgeForge'
 import { mintAdminBadge } from '../helpers/mintAdminBadge'
-import { Addresses } from 'common'
 import { logger } from '../../helpers'
+import { config } from '../../config'
 
 mintAdminBadge({
-  adminBadgeAddress: Addresses(2).badges.adminBadgeAddress,
-  superAdminBadgeAddress: Addresses(2).badges.superAdminBadgeAddress,
+  adminBadgeAddress: config.radQuest.badges.adminBadgeAddress,
+  superAdminBadgeAddress: config.radQuest.badges.superAdminBadgeAddress,
   amount: 1
 }).then(() =>
   newHeroBadgeForge().map(({ heroBadgeForgeAddress }) => logger.debug({ heroBadgeForgeAddress }))
