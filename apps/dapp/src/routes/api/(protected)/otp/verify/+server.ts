895484f6b803281415e2491e97cb2022b672c7db
Dawid Sowa <dawid.sowa@rdx.works>
2024-08-02 15:56:10 +0200

    fix(dapp): do fraud check on SMS verification

--- apps/dapp/src/routes/api/(protected)/otp/verify/+server.ts
+++ apps/dapp/src/routes/api/(protected)/otp/verify/+server.ts
@@ -1,9 +1,19 @@
 import type { RequestHandler } from './$types'
 import { routeHandler } from '$lib/server/route-handler'
+import { config } from '$lib/config'
+import { error } from '@sveltejs/kit'
 
 /** @type {import('./$types').RequestHandler} */
 export const POST: RequestHandler = async ({ request, locals }) => {
   const requestBody = await request.json()
+
+  if (config.dapp.maxUserPerIp !== 0) {
+    const preventFraudResult = await locals.controllers.authController.preventFraud(locals.clientIp)
+
+    if (preventFraudResult.isErr()) {
+      return error(preventFraudResult.error.httpResponseCode, preventFraudResult.error.reason)
+    }
+  }
 
   return routeHandler(() =>
     locals.controllers.oneTimePasswordController.verifyOneTimePassword(
--- apps/dapp/src/routes/api/auth/login/+server.ts
+++ apps/dapp/src/routes/api/auth/login/+server.ts
@@ -1,17 +1,9 @@
-import { config } from '$lib/config'
 import type { RequestHandler } from './$types'
 import { error, json } from '@sveltejs/kit'
 
 /** @type {import('./$types').RequestHandler} */
 export const POST: RequestHandler = async ({ request, cookies, locals }) => {
   const requestBody = await request.json()
-
-  if (config.dapp.maxUserPerIp != 0) {
-    const preventFraudResult = await locals.controllers.authController.preventFraud(locals.clientIp)
-
-    if (preventFraudResult.isErr())
-      error(preventFraudResult.error.httpResponseCode, preventFraudResult.error.reason)
-  }
 
   const result = await locals.controllers.authController.login(locals.context, requestBody, cookies)
 
