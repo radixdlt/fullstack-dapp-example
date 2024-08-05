dcd152dfe6440227fa6dd6d1d1ef16fcbaed7a7b
Azizi <64193693+azizi-a@users.noreply.github.com>
2024-08-05 13:07:12 +0100

    code(wallet): deploy quest-rewards-v2 to stokenet

---
+++ packages/typescript-wallet/src/quest-rewards-v2/helpers/newQuestRewardsV2.ts
@@ -0,0 +1,58 @@
+import { config } from '../../config'
+import { transactionBuilder } from '../../transaction/transactionBuilder'
+
+export const newQuestRewardsV2 = () => {
+  const transactionManifest = `
+CALL_METHOD
+    Address("${config.radQuest.accounts.owner.address}")
+    "create_proof_of_amount"
+    Address("${config.radQuest.badges.superAdminBadgeAddress}") 
+    Decimal("1")
+; 
+CALL_METHOD
+    Address("${config.radQuest.accounts.system.address}")
+    "withdraw"
+    Address("${config.radQuest.badges.adminBadgeAddress}")
+    Decimal("1")
+;
+TAKE_ALL_FROM_WORKTOP
+    Address("${config.radQuest.badges.adminBadgeAddress}")
+    Bucket("admin_badge")
+;
+CALL_FUNCTION
+    Address("${config.radQuest.questRewardsV2Package}")
+    "QuestRewardsV2"
+    "new"
+    Address("${config.radQuest.badges.superAdminBadgeAddress}")
+    Enum<OwnerRole::Fixed>(
+        Enum<AccessRule::Protected>(
+            Enum<AccessRuleNode::ProofRule>(
+                Enum<ProofRule::Require>(
+                    Enum<ResourceOrNonFungible::Resource>(
+                        Address("${config.radQuest.badges.superAdminBadgeAddress}")
+                    )
+                )
+            )
+        )
+    )
+    Address("${config.radQuest.accounts.jetty.address}")
+    Bucket("admin_badge")
+    Address("${config.radQuest.badges.heroBadgeAddress}")
+    Address("${config.radQuest.badges.kycBadgeAddress}")
+    Address("${config.radQuest.resources.clamAddress}")
+;
+`
+
+  const transaction = transactionBuilder({
+    transactionManifest,
+    signers: ['owner', 'system']
+  })
+  return transaction
+    .submit()
+    .andThen(({ transactionId }) => transaction.helper.getCreatedEntities(transactionId))
+    .map(
+      (createdEntities): Record<string, string> => ({
+        QuestRewardsV2: createdEntities[0].entity_address!
+      })
+    )
+}
