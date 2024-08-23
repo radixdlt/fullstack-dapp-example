41cab637fc77844bcca4a89305465580e963160b
Azizi <64193693+azizi-a@users.noreply.github.com>
2024-08-23 09:24:17 +0100

    feat(dapp): country select component

---
+++ apps/dapp/src/lib/components/country-select/CountrySelect.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import countries from './countries.json'
+  import { onMount } from 'svelte'
+  import Svelecte from 'svelecte'
+
+  export let country: string
+
+  onMount(async () => {})
+</script>
+
+<Svelecte
+  class="country-select"
+  bind:value={country}
+  options={countries}
+  placeholder="Select a country"
+/>
+
+<style lang="scss">
+  :global(.country-select) {
+    --sv-border: var(--border) var(--color-neutral);
+    --sv-border-radius: var(--border-radius-lg);
+    --sv-min-height: 3rem;
+    --sv-dropdown-width: min(calc(400px - 4rem), calc(100vw - 6rem));
+    --sv-bg: var(--color-light);
+  }
+</style>
