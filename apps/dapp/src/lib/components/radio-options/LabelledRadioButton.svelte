90f6796a59fcb0957a16792104732522476e6e40
Azizi <64193693+azizi-a@users.noreply.github.com>
2024-08-23 13:38:51 +0100

    feat(dapp): radio options component

---
+++ apps/dapp/src/lib/components/radio-options/LabelledRadioButton.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+  export let name: string
+  export let value: string
+  export let title: string
+  export let description: string
+  export let available: boolean | undefined = undefined
+  export let availableText:
+    | {
+        available: string
+        notAvailable: string
+      }
+    | undefined = undefined
+</script>
+
+<div class="get-xrd-method">
+  <input type="radio" {name} {value} id={name + '_' + value} />
+  <label for={name + '_' + value}>
+    <p><strong>{title}</strong></p>
+    <p>{description}</p>
+    {#if availableText && available}
+      <p style="color:orange">{availableText?.available}</p>
+    {:else if availableText && !available}
+      <p style="color:red">{availableText?.notAvailable}</p>
+    {/if}
+  </label>
+</div>
+
+<style lang="scss">
+  .get-xrd-method {
+    display: flex;
+    align-items: center;
+    border-bottom: 1px solid var(--color-neutral);
+  }
+
+  input {
+    position: relative;
+    height: 1em;
+    width: 1em;
+    background-color: var(--color-neutral);
+    border-radius: 50%;
+    border: 1px solid var(--color-dark-translucent);
+    margin-right: 1rem;
+
+    &:hover {
+      opacity: 0.8;
+    }
+
+    &:checked {
+      border: 1.5px solid var(--color-primary);
+    }
+
+    &:after {
+      content: '';
+      position: absolute;
+      display: none;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 0.5em;
+      height: 0.5em;
+      border-radius: 50%;
+      background: var(--color-primary);
+    }
+
+    &:checked:after {
+      display: block;
+    }
+  }
+</style>
---
+++ apps/dapp/src/lib/components/radio-options/RadioOptions.svelte
@@ -0,0 +1,27 @@
+<script lang="ts" context="module">
+  export interface LabelledRadioButtonArgs {
+    value: string
+    title: string
+    description: string
+    available?: boolean
+  }
+</script>
+
+<script lang="ts">
+  import LabelledRadioButton from './LabelledRadioButton.svelte'
+
+  export let name: string
+  export let options: LabelledRadioButtonArgs[]
+  export let availableText:
+    | {
+        available: string
+        notAvailable: string
+      }
+    | undefined = undefined
+</script>
+
+<div class="get-xrd">
+  {#each options as option}
+    <LabelledRadioButton {...option} {name} {availableText} />
+  {/each}
+</div>
