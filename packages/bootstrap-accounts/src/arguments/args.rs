35dc71282c8c0e1797c060166b0ed38bdb9765d6
shambupujar <shambu.pujar@rdx.works>
2024-11-30 20:45:03 +0000

    feat(core): creation of access controller and creation of file output from the process

--- packages/bootstrap-accounts/src/arguments/args.rs
+++ packages/bootstrap-accounts/src/arguments/args.rs
@@ -1,5 +1,8 @@
 use clap::{command, ArgAction, ValueEnum};
 use clap::{Args, Parser, Subcommand};
+use scrypto::types::ComponentAddress;
+
+use crate::transactions::access_controller::check_component;
 
 #[derive(Debug, Parser)]
 #[clap(author, version, about)]
@@ -13,7 +16,9 @@ pub struct Cli {
 
 #[derive(Debug, Args)]
 pub struct CreateAccessControllerCommand {
-    // Define the fields for CreateAccessControllerCommand here
+    /// Account that is paying fees for creation of access controller
+    #[arg(short , long, required=true, value_parser = check_component)]
+    pub account_address: ComponentAddress,
 }
 
 #[derive(Debug, Args)]
@@ -21,10 +26,17 @@ pub struct KeyPairsCommand {
     // Define the fields for KeyPairsCommand here
 }
 
+#[derive(Debug, Args)]
+pub struct TransactionAffectedEntitiesCommand {
+    #[arg(short, long, required = true)]
+    pub transaction_id: String,
+}
+
 #[derive(Debug, Subcommand)]
 pub enum Command {
-    
     KeyPairs(KeyPairsCommand),
 
     CreateAccessControllers(CreateAccessControllerCommand),
+
+    TransactionAffectedEntities(TransactionAffectedEntitiesCommand),
 }
